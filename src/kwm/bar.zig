gnuunixchad/kwm

commit ed56af70f96de38f5e6937c14ea92db5cd754c40

indicate sticky window on bar

src/kwm/bar.zig
@@ -367,6 +367,10 @@ fn render_static_component(self: *Self) void {
         var it = context.windows.safeIterator(.forward);
         while (it.next()) |window| {
             if (window.output == self.output) {
+                if (window.sticky) {
+                    windows_tag = @as(u32, @intCast(0)) -% 1;
+                    break;
+                }
                 windows_tag |= window.tag;
             }
         }

src/kwm/window.zig
@@ -400,6 +400,10 @@ pub fn toggle_sticky(self: *Self) void {
     log.debug("<{*}> toggle sticky: {}", .{ self, !self.sticky });
 
     self.sticky = !self.sticky;
+
+    if (comptime build_options.bar_enabled) {
+        if (self.output) |output| output.bar.damage(.tags);
+    }
 }
 
 

commit 5a619b5520756d44bc7d67b4dbe1bca18ea3c0f5

Fix(bar): box on non-occupied tags with sticky
This fixes an issue where boxes are drawn to all tags if sticky window
exists, introduced in commit ed56af70f96de38f5e6937c14ea92db5cd754c40.

src/kwm/bar.zig
@@ -376,10 +376,6 @@ fn render_static_component(self: *Self) void {
         var it = context.windows.safeIterator(.forward);
         while (it.next()) |window| {
             if (window.output == self.output) {
-                if (window.sticky) {
-                    windows_tag = @as(u32, @intCast(0)) -% 1;
-                    break;
-                }
                 windows_tag |= window.tag;
             }
         }