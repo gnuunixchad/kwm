a4add56b7eaae8bb2d4e4932be67ab32f537f057
nate zhou <gnuunixchad@outlook.com>
2026-03-13 11:16:18 +0800

    Revert "Merge branch 'feat-focus-master-return'"
    This reverts commit 54c18cedeb93302b6d6816362accc26d2aa449d1, reversing
    changes made to 57d0093e5ca29bdfec198f6311f59ea198231126.

--- src/kwm/context.zig
+++ src/kwm/context.zig
@@ -433,15 +433,6 @@ pub fn focus(self: *Self, window: *Window) void {
 
     if (window.output) |output| {
         self.set_current_output(output);
-
-        var is_master = true;
-        var it = self.windows.safeIterator(.forward);
-        while (it.next()) |w| {
-            if (w.is_visible_in(output) and !w.floating) {
-                is_master = (w == window);
-                break;
-            }
-        }
     }
 
     window.flink.remove();
